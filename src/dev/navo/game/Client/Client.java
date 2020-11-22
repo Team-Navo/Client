13d25b7d302a1aa12a91dc5cefa7461157918668
EunsilSon <eunsil1023@gmail.com>
2020-11-22 15:35:04 +0900

    Authentication 이중 Json

--- src/dev/navo/game/Client/Client.java
+++ src/dev/navo/game/Client/Client.java
@@ -8,8 +8,6 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import org.json.simple.JSONObject;
 
-import java.io.IOException;
-
 @SuppressWarnings("unchecked")
 public class Client {
     private static Client instance;
@@ -17,14 +15,16 @@ public class Client {
     static final int PORT = Integer.parseInt(System.getProperty("port", "1120"));
     Channel channel;
     Buffer buffer;
-    Buffer inputBuffer;
     String owner; //로그인 한 아이디
+
     public void setOwner(String owner) {
         this.owner = owner;
     }
+
     public String getOwner() {
         return this.owner;
     }
+
     public Client(){
         buffer = new Buffer();
 
@@ -45,70 +45,97 @@ public class Client {
             System.out.println("설정 종료");
         }
     }
+
     public static Client getInstance(){
         if(instance == null)
             instance = new Client();
         return instance;
     }
+
     // SIGN IN
-    public boolean login(String id, String pw) throws IOException {
+    public boolean login(String id, String pw) {
 
-        JSONObject dupJson = new JSONObject();
-        dupJson.put("Header", "1");
-        dupJson.put("id", id);
-        dupJson.put("pw", pw);
+        JSONObject body = new JSONObject();
+        body.put("Header", "1");
+        body.put("id", id);
+        body.put("pw", pw);
 
-        JSONObject mainJson = new JSONObject();
-        mainJson.put("Auth", "1");
-        mainJson.put("Body", dupJson.toJSONString());
+        JSONObject header = new JSONObject();
+        header.put("Auth", "1");
+        header.put("Body", body.toJSONString());
 
-        System.out.println(mainJson.toJSONString());
-        channel.writeAndFlush(mainJson.toJSONString() + "\n");
+        System.out.println(header.toJSONString());
+        channel.writeAndFlush(header.toJSONString() + "\n");
 
         String recvData = buffer.get();
         return recvData.equals("SUCCESS");
     }
+
     // SIGN UP
-    public boolean create(String id, String pw, String name, String birth, String phone) throws IOException {
-        JSONObject json = new JSONObject();
-        json.put("Header", "2");
-        json.put("id", id);
-        json.put("pw", pw);
-        json.put("name", name);
-        json.put("birth", birth);
-        json.put("phone", phone);
-        System.out.println(json.toJSONString());
-        channel.writeAndFlush(json.toJSONString() + "\n");
+    public boolean create(String id, String pw, String name, String birth, String phone) {
+
+        JSONObject body = new JSONObject();
+        body.put("Header", "2");
+        body.put("id", id);
+        body.put("pw", pw);
+        body.put("name", name);
+        body.put("birth", birth);
+        body.put("phone", phone);
+
+        JSONObject header = new JSONObject();
+        header.put("Auth", "2");
+        header.put("Body", body.toJSONString());
+
+        System.out.println(header.toJSONString());
+        channel.writeAndFlush(header.toJSONString() + "\n");
+
         String recvData = buffer.get();
         return recvData.equals("SUCCESS");
     }
+
     // FIND ID
-    public String idFind(String name, String birth) throws IOException {
-        JSONObject json = new JSONObject();
-        json.put("Header", "3");
-        json.put("name", name);
-        json.put("birth", birth);
-        System.out.println(json.toJSONString());
-        channel.writeAndFlush(json.toJSONString() + "\n");
+    public String idFind(String name, String birth) {
+
+        JSONObject body = new JSONObject();
+        body.put("Header", "3");
+        body.put("name", name);
+        body.put("birth", birth);
+
+        JSONObject header = new JSONObject();
+        header.put("Auth", "3");
+        header.put("Body", header.toJSONString());
+
+        System.out.println(header.toJSONString());
+        channel.writeAndFlush(header.toJSONString() + "\n");
+
         String result = buffer.get();
+
         if(result.equals("FAIL"))
             return null;
         else
             return result;
     }
+
     // FIND PW
-    public String pwFind(String id, String name) throws IOException {
+    public String pwFind(String id, String name) {
+
         JSONObject json = new JSONObject();
         json.put("Header", "4");
         json.put("id", id);
         json.put("name", name);
-        System.out.println(json.toJSONString());
-        channel.writeAndFlush(json.toJSONString() + "\n");
+
+        JSONObject header = new JSONObject();
+        header.put("Auth", "4");
+        header.put("Body", header.toJSONString());
+
+        System.out.println(header.toJSONString());
+        channel.writeAndFlush(header.toJSONString() + "\n");
+
         String result = buffer.get();
+
         if(result.equals("FAIL"))
             return null;
         else
             return result;
     }
 }
-
